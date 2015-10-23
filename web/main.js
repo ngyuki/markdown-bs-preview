7d7a711b54f65fe4e30f2f0720818d7154f60b29
ngyuki <ngyuki.jp@gmail.com>
2015-10-24 01:01:20 +0900

    fix: browser-sync.js is async script tag

--- web/main.js
+++ web/main.js
@@ -1,16 +1,20 @@
 $(function(){
     $.get('!markdown-body?' + location.pathname).then(function(data){
         $('#markdown-body').html(data);
-    })
+    });
 
-    setTimeout(function(){
-        var io = ___browserSync___.socket;
+    (function(){
+        if (typeof(___browserSync___) === 'undefined') {
+            setTimeout(arguments.callee, 100);
+            console.log('timeput');
+        } else {
+            var io = ___browserSync___.socket;
 
-        io.on('markdown:change', function(data){
-            $.get('!markdown-body?' + location.pathname).then(function(data){
-                $('#markdown-body').html(data);
-            })
-        });
-
-    }, 1);
+            io.on('markdown:change', function(data){
+                $.get('!markdown-body?' + location.pathname).then(function(data){
+                    $('#markdown-body').html(data);
+                })
+            });
+        }
+    }());
 });
